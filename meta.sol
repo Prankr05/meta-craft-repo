79b7722c13cf4914ee6755454bc4097baa2a3a5f
Prankr05 <149897496+Prankr05@users.noreply.github.com>
2023-11-05 00:39:04 +0530

    meta.sol

--- meta.sol
+++ meta.sol
@@ -2,18 +2,21 @@
 pragma solidity 0.8.18;
 
 contract MyToken {
+    // public variables here
     string public tokenName = "Priyranjan";
     string public tokenAbbrv = "Kumar";
     uint public totalSupply = 0;
 
+    // mapping variable here
     mapping(address => uint) public balances;
 
-
+     // mint function
     function mint(address account, uint value) public {
             totalSupply += value;
             balances[account] += value;
     }
 
+     // burn function
     function burn(address account, uint value) public {
         if (balances[account] >= value) {
             totalSupply -= value;
@@ -21,4 +24,5 @@ contract MyToken {
         }
     }
 }
+
 
