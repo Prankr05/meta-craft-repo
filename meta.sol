29fe8f134fa594307212fcb0babdae00e8dba40c
Prankr05 <149897496+Prankr05@users.noreply.github.com>
2023-11-05 00:33:39 +0530

    meta.sol

--- meta.sol
+++ meta.sol
@@ -4,31 +4,21 @@ pragma solidity 0.8.18;
 contract MyToken {
     string public tokenName = "Priyranjan";
     string public tokenAbbrv = "Kumar";
-    uint256 public totalSupply = 0;
+    uint public totalSupply = 0;
 
-    mapping(address => uint256) public balances;
+    mapping(address => uint) public balances;
 
-    constructor() {
-        balances[msg.sender] = totalSupply;
-    }
 
-    function mint(address account, uint256 value) public {
-        if (account == address(0)) {
-            revert("Invalid address");
-        } else {
+    function mint(address account, uint value) public {
             totalSupply += value;
             balances[account] += value;
-        }
     }
 
-    function burn(address account, uint256 value) public {
-        if (account == address(0)) {
-            revert("Invalid address");
-        } else if (balances[account] < value) {
-            revert("Insufficient balance to burn");
-        } else {
+    function burn(address account, uint value) public {
+        if (balances[account] >= value) {
             totalSupply -= value;
             balances[account] -= value;
         }
     }
 }
+
